00fda64f99a1a8554f4727d70817fb9de3259171
MolybdenumLee <limu.robert@outlook.com>
2021-01-24 17:49:33 +0800

    write a hollow shphere model for template

--- mathmodel_template.py
+++ mathmodel_template.py
@@ -2,51 +2,44 @@
 
 import numpy as np
 
-# to generate a 3D model from a mathematical description
-# for example: a hollow sphere is "x**2+y**2+z**2 >= R1**2 and x**2+y**2+z**2 <= R2**2
-# also, in spherical coordinates, a hollow sphere is r >=R1 and r <= R2
-#
-# function recieve an array of n points, shape = (n, 3)
-#
-# function must return an array with shape = (n,)
-# each figure in this array cooresponds to a point in the input array
-# in the output array, 1 means the the point is in the model,
-# 0 means the point is not in the model
-#
-# boundaryList is 
-#   - [xmin, xmax, ymin, ymax, zmin, zmax]
-# coord is 
-#   - 'xyz' |in (x,y,z)
-#   - 'sph' |in (r, theta, phi)|theta: 0~pi ; phi: 0~2pi
-#   - 'cyl' |in (r, phi, z)|phi:0-2pi
-# coord must be the last arguement
+'''
+to generate a 3D model from a mathematical description
+for example: a hollow sphere is "x**2+y**2+z**2 >= R1**2 and x**2+y**2+z**2 <= R2**2
+also, in spherical coordinates, a hollow sphere is r >=R1 and r <= R2
 
+coord is 
+  - 'xyz' |in (x, y, z)
+  - 'sph' |in (r, theta, phi) |theta: 0~2pi ; phi: 0~pi
+  - 'cyl' |in (rho, phi, z) |theta:0-2pi
+'''
 
-
-# function name must be model !
-# do not change the keyword arguments named 'coord' and 'boundry_xyz' !
-# boundry_xyz is a list in format [xmin, xmax, ymin, ymax, zmin, zmax]
-def model(points_xyz, param1=30, param2=60, param3=90, boundary_xyz=[-100,100,-100,100,-100,100], coord='xyz'):
-    pass
-
-
+# Don't change the class name, attributes name or method name !
 class specific_mathmodel:
+    '''A template of hollow sphere math model
+    '''
 
     def __init__(self):
         self.params = {
-            'param1': 30,
-            'param2': 60,
-            'param3': 90
+            'R1': 10,
+            'R2': 15
         }
-        self.boundary_min = np.array([-100, -100, -100])
-        self.boundary_max = np.array([100, 100, 100])
-        self.coord = 'xyz'  # 'xyz' or 'sph' or 'cyl'
+        self.boundary_min = -self.params['R2']*np.ones(3)
+        self.boundary_max = self.params['R2']*np.ones(3)
+        self.coord = 'sph'  # 'xyz' or 'sph' or 'cyl'
 
     def shape(self, grid_in_coord):
-        pass
+        points_sph = grid_in_coord
+        R1 = self.params['R1']
+        R2 = self.params['R2']
+
+        in_model_grid_index = np.zeros(points_sph.shape[0])
+        r = points_sph[:, 0]
+        R1_array = R1 * np.ones_like(r)
+        R2_array = R2 * np.ones_like(r)
+        in_model_grid_index = np.sign(r-R1_array) * np.sign(R2_array-r)
+        in_model_grid_index = np.sign(in_model_grid_index+1)
+        self.in_model_grid_index = in_model_grid_index
         return self.in_model_grid_index
 
     def sld(self):
-        pass
-        return 1 * self.in_model_grid_index
-        # return the sld of each point
+        return 15 * self.in_model_grid_index
