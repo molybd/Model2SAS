d865e4567a75c710a1117ed5db6f3d8fc12bb34f
limu <limu.robert@outlook.com>
2022-08-18 15:00:16 +0800

    change the pararmeters used

--- mathmodel_template.py
+++ mathmodel_template.py
@@ -12,31 +12,32 @@ import numpy as np
 # =========================================================
 class SpecificMathModel:
     '''to generate a 3D model from a mathematical description
-    for example: a hollow sphere is "x**2+y**2+z**2 >= R1**2 and x**2+y**2+z**2 <= R2**2
-    also, in spherical coordinates, a hollow sphere is r >=R1 and r <= R2
+    for example: a spherical shell is "x**2+y**2+z**2 >= R_core**2 and x**2+y**2+z**2 <= (R_core+thickness)**2
+    also, in spherical coordinates, a hollow sphere is (r >= R_core) and (r <= R_core+thickness)
 
     coord:
     - 'car' |in (x, y, z)
     - 'sph' |in (r, theta, phi) |theta: 0~2pi ; phi: 0~pi
     - 'cyl' |in (rho, phi, z) |theta:0-2pi
     '''
-    def __init__(self):
+    def __init__(self) -> None:
         '''must at least have these 2 attributes
         '''
         self.params = {
-            'R1': 8,
-            'R2': 10
+            'R_core': 40,
+            'thickness': 10
         }
         self.coord = 'sph'  # 'car' or 'sph' or 'cyl'
 
-    def get_boundary(self):
-        '''re-generate boundary in case that params are altered in software
+    def get_boundary(self) -> tuple:
+        '''re-generate boundary for every method call
+        in case that params are altered in software
         '''
-        boundary_min = -self.params['R2']*np.ones(3)
-        boundary_max = self.params['R2']*np.ones(3)
+        boundary_max = (self.params['R_core']+self.params['thickness']) * np.ones(3)
+        boundary_min = -boundary_max
         return boundary_min, boundary_max
 
-    def sld(self, grid_points_in_coord):
+    def sld(self, grid_points_in_coord:np.ndarray) -> np.ndarray:
         ''' calculate sld values of each grid points
         Args:
             grid_points_in_coord: ndarray, shape == (n, 3), value is in the coordinates of self.coord
@@ -45,10 +46,11 @@ class SpecificMathModel:
         '''
         points_sph = grid_points_in_coord
         r, theta, phi = points_sph[:,0], points_sph[:,1], points_sph[:,2]
-        R1 = self.params['R1']
-        R2 = self.params['R2']
+        R = self.params['R_core']
+        t = self.params['thickness']
 
         index = np.zeros_like(r)
-        index[(r>=R1) & (r<=R2)] = 1
-        sld = r**2 * index
-        return sld
+        index[(r>=R) & (r<=(R+t))] = 1
+        #grid_sld = r * np.cos(theta) * np.sin(phi) * index
+        grid_sld = 1 * index
+        return grid_sld
